980b8316d14568fe2a536d758fcccd281bbb6923
spopov812 <aleksandrspopov812@gmail.com>
2020-02-12 16:08:01 -0500

    Updated CController comments

--- src/Utils/cloud.py
+++ src/Utils/cloud.py
@@ -1,24 +1,39 @@
 from google.cloud import storage
 
+"""
+Class that handles interfacing with Google Cloud storage buckets which contain PPMI
+image data that will be used for models.
+"""
 class CController:
 
-	def __init__(self, key_path, bucket_name):
+    """
+    Contstructor. Needs to have key to access bucket and the name of the bucket itself.
+    """
+    def __init__(self, key_path, bucket_name):
 
-		self.key_path = key_path
-		self.bucket_name = bucket_name
+        self.key_path = key_path
+        self.bucket_name = bucket_name
 
-		self.storage_client = storage.Client.from_service_account_json(self.key_path)
+        # Requests access
+        self.storage_client = storage.Client.from_service_account_json(self.key_path)
 
-		self.bucket = self.storage_client.bucket(self.bucket_name)
+        self.bucket = self.storage_client.bucket(self.bucket_name)
 
-	def download_blob(self, filename, destination):
+    """
+    Downloads file (blob) from the filename in the bucket to target destination locally.
+    """
+    def download_blob(self, filename, destination):
     
-		blob = self.bucket.blob(filename)
-		blob.download_to_filename(destination)
+        blob = self.bucket.blob(filename)
+        blob.download_to_filename(destination)
 
-	def get_blob_iterator(self, prefix="raw_data/", delimiter=""):
+    """
+    Creates iterator (python generator) which can be used to get information about all data
+    in the bucket.
+    """
+    def get_blob_iterator(self, prefix="raw_data/", delimiter=""):
 
-		return self.storage_client.list_blobs(
-			self.bucket_name, 
-			prefix=prefix, 
-			delimiter=delimiter)
+        return self.storage_client.list_blobs(
+            self.bucket_name, 
+            prefix=prefix, 
+            delimiter=delimiter)
